a1b1770743aa0e05187ba9d771aa15127d09565d
Thiago <thiagos2000@gmail.com>
2025-12-23 17:29:31 -0300

    feat/removendo gamescene sks

--- RPGMinecraft/GameViewController.swift
+++ RPGMinecraft/GameViewController.swift
@@ -13,43 +13,28 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+
+        if let skView = self.view as? SKView {
+            let scene = GameScene(size: skView.bounds.size)
+            scene.scaleMode = .resizeFill
+
+            skView.presentScene(scene)
+
+            skView.ignoresSiblingOrder = true
+            skView.showsFPS = true
+            skView.showsNodeCount = true
         }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .portrait
         } else {
             return .all
         }
     }
 
     override var prefersStatusBarHidden: Bool {
-        return true
+        return false
     }
 }
